292e23a2e68d11ad73d6a9f40664b5fdf443ebfa
veryWhiteRice <130551358+veryWhiteRice@users.noreply.github.com>
2023-06-07 01:05:31 +0900

    Update JH_TCP_ServerDlg.h

--- Server/JH_TCP_Server/JH_TCP_ServerDlg.h
+++ Server/JH_TCP_Server/JH_TCP_ServerDlg.h
@@ -1,4 +1,4 @@
-﻿
+
 // JH_TCP_ServerDlg.h: 헤더 파일
 //
 
@@ -9,20 +9,20 @@
 #include "afxcoll.h"
 struct ThreadArg
 {
-	CStringList* pList;
+	CStringList* pList;//Message 담는 List형 함수
 	CDialogEx* pDlg;
-	int Thread_run;
+	int Thread_run;// Thread_run 실행
 };
-class CDataSocket;
-class CListenSocket;
+class CDataSocket;// Client에게 Message 전달하는 용도
+class CListenSocket; // Server 여는 Socket
 // CJHTCPServerDlg 대화 상자
 class CJHTCPServerDlg : public CDialogEx
 {
 // 생성입니다.
 public:
 	CJHTCPServerDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
-	CListenSocket* m_pListenSocket;
-	CDataSocket* m_pDataSocket;
+	CListenSocket* m_pListenSocket; // 서버 Socket
+	CDataSocket* m_pDataSocket; // Client Socket
 // 대화 상자 데이터입니다.
 #ifdef AFX_DESIGN_TIME
 	enum { IDD = IDD_JH_TCP_SERVER_DIALOG };
@@ -44,10 +44,9 @@ protected:
 	DECLARE_MESSAGE_MAP()
 public:
 	CEdit m_rx_edit;
-	CWinThread* pThread1, * pThread2;
+	CWinThread* pThread1, * pThread2; // Thread 실행 객체
 	ThreadArg arg1, arg2;//쓰레드 함수
-	void ProcessAccept(int nErrorCode);
-	void ProcessReceive(CDataSocket* pSocket, int nErrorCode);
-	void ProcessClose(CDataSocket* pSocket, int nErrorCode);
-	void ProcessSend(CDataSocket* pSocket, int nErrorCode);
+	void ProcessAccept(int nErrorCode); // Socket Connect 요청을 받는 메서드
+	void ProcessReceive(CDataSocket* pSocket, int nErrorCode); // Socket Message를 받는 메서드
+	void ProcessClose(CDataSocket* pSocket, int nErrorCode); // Socket Close 메서드
 };
